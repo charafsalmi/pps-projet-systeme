6da0376227259b95c28a459799c28d1f80c5c25a
francois.haurit <francois.haurit@5da52056-f020-11dd-b507-8b4ecd5210be>
2009-02-05 22:20:38 +0000

    Le processus Direction crée ses petits collègues puis attends la mort de Accueil, dis à Administration de se suicider puis se suicide à son tour...

--- process_pdirection.c
+++ process_pdirection.c
@@ -18,18 +18,22 @@ int main(int nbarg, char *tbarg[])
 	/*
 	 * Lance pAccueil en premier pour lui donner le pid de pAdministration
 	 */
+	int Tadmin_accu[2],
+	    Taccu_guichet[2];
+	pipe(Tadmin_accu);
+	pipe(Taccu_guichet);
 	if((pidAccueil = fork()) < 0)
 	{
 		perror("Erreur dans la création du processus Paccueil.");
 		exit(1);
 	}
-	else
-		if(pidAccueil == 0)
+	else if(pidAccueil == 0)
 		{
 			/*
 			 * Bienvenue dans le processus fils Paccueil.
 			 */
-			//execl();
+			close(Tadmin_accu[1]);
+			execl("pAccueil","pAccueil",Tadmin_accu[0],Taccu_guichet[0],Taccu_guichet[1],NULL);
 		}
 
 	/*
@@ -40,14 +44,19 @@ int main(int nbarg, char *tbarg[])
 		perror("Erreur dans la création du processus Padministration.");
 		exit(errno);
 	}
-	else
-		if(pidAdministration == 0)
+	else if(pidAdministration == 0)
 		{
 			/*
 			 * Bienvenue dans le processus fils Padministration.
 			 */
-			//execl();
+			close(Tadmin_accu[0]);
+			close(Taccu_guichet[0]);
+			close(Taccu_guichet[1]);
+			execl("pAdmin","pAdmin",Tadmin_accu[1],pidAccueil,NULL);
 		}
-
+	int e;
+	wait(&e);
+	kill(SIGUSR1,pidAdministration);
+	wait(&e);
 	return 0;
 }
