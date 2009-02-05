52c3b195a98afffdef9e9f8f95ef77bc702b2bd2
salmi.charaf <salmi.charaf@5da52056-f020-11dd-b507-8b4ecd5210be>
2009-02-05 03:30:40 +0000

    Tout compile, c'est bien, mais ça marche paaaaaas :'(

--- process_paccueil.c
+++ process_paccueil.c
@@ -7,12 +7,42 @@
 
 #include "type_definitions.h"
 
-int main()
+int main(int nbarg, char *tbarg[])
 {
+	/*
+	 * Compteurs et buffers
+	 */
+	int i = 0;
+	char* cbuffer;
+
+	/*
+	 * Paramètres
+	 */
+	//Nombre de guichets voulus
+	int nb_guichets = 0;
+
 	/*
 	 * Début du programme
 	 */
-
+	for(i = 0; i < nb_guichets; ++i)
+	{
+		if((pidGuichet[i] = fork()) < 0)
+		{
+			sprintf(cbuffer,
+					"Erreur dans la création du processus Pguichet[%d].", i);
+			perror(cbuffer);
+			exit(errno);
+		}
+		else
+			if(pidGuichet[i] == 0)
+			{
+				/*
+				 * Bienvenue dans le processus fils Pguichet[i]
+				 */
+				//execl("process_guichet");
+				exit(1);
+			}
+	}
 
 	/*
 	 * Fin du programme
